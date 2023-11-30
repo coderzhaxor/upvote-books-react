44443a192c549ed9342af309703d0bf47639009c
coderzhaxor <ajanuantara@gmail.com>
2023-11-30 07:25:37 +0700

    Add login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,13 +1,44 @@
 import CircleKiri from '../../assets/img/circle_kiri.png';
 import CircleKanan from '../../assets/img/circle_kanan.png';
 
+import Logo from '../../assets/svg/logo-animation.svg';
+
 export default function Login() {
     return (
-        <main className="relative min-h-screen w-full">
-            <img src={CircleKiri} className="absolute top-0 left-0" />
-            <img src={CircleKanan} className="absolute top-0 right-0" />
-            <img src={CircleKiri} className="absolute bottom-0 right-0 rotate-180" />
-            <img src={CircleKanan} className="absolute bottom-0 left-0 rotate-180" />
+        <main className="relative min-h-screen w-full items-center justify-center flex">
+            <img src={CircleKiri} className="absolute top-0 left-0 -mt-20 select-none" />
+            <img src={CircleKanan} className="absolute top-0 right-0 -mt-20 select-none" />
+            <img src={CircleKiri} className="absolute bottom-0 right-0 rotate-180 -mb-20 select-none" />
+            <img src={CircleKanan} className="absolute bottom-0 left-0 rotate-180 -mb-20 select-none" />
+
+            <div className="w-[42rem] h-[46rem] absolute blur-3xl opacity-20 -z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-blue-500 rounded-full"></div>
+
+            <div className="max-w-3xl flex flex-col bg-[#1C1C65]/60 p-11 justify-center rounded-lg">
+                <div className="logo flex justify-center">
+                    <img src={Logo} width={142} />
+                </div>
+                <h1 className="font-semibold text-2xl my-8">Welcome to Upvote Books</h1>
+                <label htmlFor="email">
+                    Email
+                    <input type="email" id="email" className="block w-full bg-white/10 py-2 px-3 mb-4 mt-2 rounded-sm outline-none ring-1 ring-white/30" placeholder="youremail@gmail.com" />
+                </label>
+                <label htmlFor="Password">
+                    Password
+                    <input type="password" id="password" className="block w-full bg-white/10 py-2 px-3 mt-2 rounded-sm ring-1 ring-white/30" placeholder="Your password" />
+                </label>
+                <a href="" className="text-end underline text-blue-400 mt-3 text-sm">
+                    Forgot password?
+                </a>
+
+                <span className="text-center mt-4">
+                    Don't have an account?{' '}
+                    <a href="" className="underline text-blue-400">
+                        Signup here
+                    </a>
+                </span>
+
+                <button className="px-5 py-2 mt-6 text-lg font-semibold bg-indigo-600 rounded-sm">Login</button>
+            </div>
         </main>
     );
 }
